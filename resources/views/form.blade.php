2a98d8758396604a342c6be0a3f4f79fa0db9fd1
zain776 <zn961126@gmail.com>
2023-12-05 17:57:43 +0500

    V1

---
+++ resources/views/form.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<style>
+    .form{
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+        height: 400px;
+        width: 400px;
+        margin: auto;
+    }
+</style>
+<body>
+    <div class="form">
+        <form action="">
+            <div><label for="Name">User Name</label></div>
+            <input type="text" placeholder="Enter your name">
+            <div><label for="Email">User Email</label></div>
+            <input type="emmail" placeholder="Enter your email">
+            <div><label for="password">User Password</label></div>
+            <input type="password" placeholder="Enter your password">
+        </form>
+    </div>
+</body>
+</html>
